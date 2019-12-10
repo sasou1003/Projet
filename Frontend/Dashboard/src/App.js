69abc5c106f540295bba7f517dedb6c75aca160b
unknown <sara.sandid@edu.ece.fr>
2019-12-10 21:51:41 +0100

    responsive ok

--- Frontend/Dashboard/src/App.js
+++ Frontend/Dashboard/src/App.js
@@ -27,29 +27,29 @@ class App extends React.Component {
                 <a>Dashboards Objets Connectés</a>
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a>&copy; 2019 | Sara Sandid & Rémi Flippo</a>
+                        <a>&copy; 2019 | Sandid & Flippo</a>
                     </li>
                 </ul>
             </nav>
             <div className="container-fluid">
                 <div className="row">
-                    <nav className="col-md-2 d-none d-md-block sidebar shadow"> {/* side bar : sa taille s'adapte a la taille de la page, il sera complet quand ta page sera remplie*/}
+                    <nav className="col-lg-2 col-md-3 col-sm-4 sidebar shadow"> {/* side bar : sa taille s'adapte a la taille de la page, il sera complet quand ta page sera remplie*/}
                         <div className="sidebar-sticky">
                             <ul>
 
-                                <li>Statistiques</li>
-                                <li>Administrateur</li>
+                                <li className="d-md-block">Statistiques</li>
+                                <li className="d-md-block">Administrateur</li>
 
                             </ul>
                         </div>
                     </nav>
-                <main className="col-10">
+                <main className="col-lg-10 col-md-9 col-sm-8">
 
 
                         <div className="row">
 
                             {/*Widget 1 : nb utilisateurs total*/}
-                                <div className="card col-6">
+                                <div className="card col-lg-6 col-md-6 col-sm-12 col-xs-12">
                                     <div className="card-title"> <strong> Température moyenne </strong> </div>
                                     <div className="card-body">
                                         <div className="vente">
@@ -59,7 +59,7 @@ class App extends React.Component {
                                 </div>
 
                             {/*Widget 2 : localisation des sensors dans la maison*/}
-                                <div className="card col-6">
+                                <div className="card col-lg-6 col-md-6 col-sm-12 col-xs-12">
                                 <div className="card-title"> <strong> Localisation des sensors dans la maison </strong> </div>
                                 <div className="card-body">
                                     <div className="vente">
@@ -83,7 +83,7 @@ class App extends React.Component {
 
                         <div className="row">
 
-                                <div className="card col-4">
+                                <div className="card col-lg-4 col-md-6 col-sm-12 col-xs-12">
                                 <div className="card-title"> <strong> Taille de la maison de nos utilisateurs </strong></div>
                                 <div className="card-body">
                                     <div className="widget5">
@@ -92,7 +92,7 @@ class App extends React.Component {
                                 </div>
                                 </div>
 
-                                <div className="card col-4">
+                                <div className="card col-lg-4 col-md-6 col-sm-12 col-xs-12">
                                 <div className="card-title"> <strong> Nombre total d'utilisateurs</strong></div>
                                 <div className="card-body">
                                     <div className="widget5">
@@ -101,7 +101,7 @@ class App extends React.Component {
                                 </div>
                                 </div>
 
-                                <div className="card col-4">
+                                <div className="card col-lg-4 col-md-12 col-sm-12 col-xs-12">
                                 <div className="card-title"> <strong> Nombre total de capteurs </strong></div>
                                 <div className="card-body">
                                     <div className="widget6">
